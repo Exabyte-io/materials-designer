13f510ac8a3a91387dce97eae36d94dea5bae2b2
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-05 13:21:49 -0800

    ignoe build dir

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './stylesheets/designer.scss';
 import {MaterialsDesignerContainer} from './MaterialsDesignerContainer';
 
 ReactDOM.render(<MaterialsDesignerContainer/>, document.getElementById('root'));
