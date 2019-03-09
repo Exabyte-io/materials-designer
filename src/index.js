95b269939f673496d56b24306b1e16306016e33d
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-08 17:45:34 -0800

    Minor cleanups

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 // Load styling, bootstrap needs to be loaded first
 import 'bootstrap/dist/css/bootstrap.css';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/stackslide.css';
+import "wave/dist/stylesheets/wave.css";
 import "./stylesheets/main.scss";
 
 import {MaterialsDesignerContainer} from './MaterialsDesignerContainer';
