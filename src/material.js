31c83ba18d2c974320de162e04b58a4f9d1b9596
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-10 16:15:31 -0700

    Remove tags and exabyteId from MD

--- src/material.js
+++ src/material.js
@@ -10,12 +10,6 @@ export class Material extends Made.Material {
 
     set id(id) {this.setProp('_id', id)}
 
-    get exabyteId() {return this.prop('exabyteId')}
-
-    get tags() {return this.prop('tags', [])}
-
-    set tags(array) {this.setProp('tags', array)}
-
     get metadata() {return this.prop('metadata', {})}
 
     set metadata(object) {this.setProp('metadata', object)}
@@ -24,17 +18,13 @@ export class Material extends Made.Material {
         return {
             ...super.toJSON(),
             _id: this.id,
-            tags: this.tags,
-            metadata: this.metadata,
-            exabyteId: this.exabyteId
+            metadata: this.metadata
         };
     }
 
     unsetProp(name) {delete this._json[name]}
 
     cleanOnCopy() {
-        [
-            "_id",
-        ].forEach(p => this.unsetProp(p));
+        ["_id"].forEach(p => this.unsetProp(p));
     }
 }
