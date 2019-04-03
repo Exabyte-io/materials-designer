52116d6fd2204adb8369b18c5c292fb76e5da47a
Mohammad Mohammadi <mohammad@exabyte.io>
2019-04-03 15:34:14 -0700

    Fix tests

--- tests/cucumber/support/step_definitions/When I exit materials designer.js
+++
@@ -1,7 +0,0 @@
-import {materialDesignerPage} from "../widgets/material_designer_page";
-
-export default function () {
-    this.When(/^I exit materials designer/, function () {
-        materialDesignerPage.designerWidget.exit();
-    });
-};
