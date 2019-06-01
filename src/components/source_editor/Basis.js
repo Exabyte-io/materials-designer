8dcf7134e259456d4d503002b90dbee222ef7375
Timur Bazhirov <timur@exabyte.io>
2019-05-31 18:18:24 -0700

    Isolate Basis Textarea component

--- src/components/source_editor/Basis.js
+++ src/components/source_editor/Basis.js
@@ -1,11 +1,11 @@
-import $ from "jquery";
+import s from "underscore.string";
 import React from 'react';
 import setClass from 'classnames';
 import {Made} from "@exabyte-io/made.js";
 import ExpandMoreIcon from 'material-ui-icons-next/ExpandMore';
 import ExpansionPanel, {ExpansionPanelSummary, ExpansionPanelDetails,} from 'material-ui-next/ExpansionPanel';
 
-import {displayMessage} from "../../i18n/messages";
+import BasisText from "./BasisText";
 
 class Basis extends React.Component {
 
@@ -13,22 +13,23 @@ class Basis extends React.Component {
         super(props);
 
         this.state = {
-            validated: true,
-            message: '',
+            xyz: this.props.material.getBasisAsXyz(),
             manualEditStarted: false,
             showBasisOptions: false,
             coordUnits: Made.ATOMIC_COORD_UNITS.crystal
         };
 
-        this.handleToggleCrystalBasis = this.handleToggleCrystalBasis.bind(this);
-        this.handleBasisUnits = this.handleBasisUnits.bind(this);
-        this.handleMaterialTextareaChange = this.handleMaterialTextareaChange.bind(this);
+        this.handleBasisTextChange = this.handleBasisTextChange.bind(this);
     }
 
-    componentDidUpdate() {
-        let material = this.props.material;
+    componentWillReceiveProps(nextProps) {
+        if (this.props.material !== nextProps.material) {
+            this.setState({xyz: nextProps.material.getBasisAsXyz()})
+        }
+    }
 
-        switch (this.state.coordUnits) {
+    getXYZInCoordUnits(material, coordUnits) {
+        switch (coordUnits) {
             case Made.ATOMIC_COORD_UNITS.cartesian:
                 material.toCartesian();
                 break;
@@ -38,86 +39,35 @@ class Basis extends React.Component {
             default:
                 break;
         }
-        this.reformatXYZText();
+        return material.getBasisAsXyz();
     }
 
-    reformatXYZText = () => {
-        const materialXYZ = this.props.material.getBasisAsXyz();
-        // Change state only if user is not editing basis
-        if (!this.state.manualEditStarted && this.state.xyz !== materialXYZ) {
-            this.setState({
-                xyz: materialXYZ
-            })
-        }
-    };
-
-    handleMaterialTextareaChange(e) {
-        const value = $(e.target).val();
-
-        this.setState({
-            xyz: value
-        }, () => {
-            if (this._validateXYZ(value)) {
-                // "clone" original material from props to assert state updates
-                const material = this.props.material.clone();
-                material.setBasis(value, 'xyz', this.state.coordUnits);
-                this.props.onUpdate(material);
-            }
-        });
+    handleBasisTextChange(content) {
+        // "clone" original material from props to assert state updates
+        const material = this.props.material.clone();
+        material.setBasis(content, 'xyz', this.state.coordUnits);
+        this.props.onUpdate(material);
     }
 
-    handleToggleCrystalBasis(e) {
-        $(e.target).toggleClass("active");
-        $(this.basisOptions).toggle();
-        $(this.basisOptionCloseBtn).toggleClass("active");
-        this.setState({
-            showBasisOptions: !this.state.showBasisOptions
-        });
-    }
+    renderBasisUnitsLabel(unitsType = "crystal") {
+        return (
+            <label
+                className={setClass(
+                    "btn btn-custom",
+                    {"active": this.state.coordUnits === unitsType}
+                )}
+                id="basis-units-crystal"
+                onClick={() => {
+                    this.setState({
+                        coordUnits: unitsType,
+                        xyz: this.getXYZInCoordUnits(this.props.material, unitsType)
+                    })
+                }}
+            >
+                {s.capitalize(unitsType)} Units
+            </label>
 
-    handleBasisUnits(e) {
-        let unitType = $(e.target).find('input').val();
-        this.setState({coordUnits: unitType});
-    }
-
-    componentDidMount() {
-        this.setState({
-            xyz: this.props.material.getBasisAsXyz()
-        });
-    }
-
-    _validateXYZ(value) {
-        try {
-            Made.parsers.xyz.validate(value);
-            // only show the success message first time after last failure
-            if (!this.state.validated) {
-                this.setState({
-                    validated: true,
-                    message: displayMessage('basis.validationSuccess')
-                });
-            } else {
-                // already validated before -> remove message
-                this.setState({message: ''});
-            }
-        } catch (err) {
-            this.setState({
-                validated: false,
-                message: displayMessage('basis.validationError')
-            });
-            return false;
-        }
-        return true;
-    }
-
-    // recalculate padding for the .xyz class to keep room for "advanced-geometry" button
-    recalculatePaddingBottom() {
-        let padding;
-//        padding = this.state.showBasisOptions ? 200 : 150;
-        padding = this.state.showBasisOptions ? 150 : 100;
-        if (this.state.message) {
-            padding += 25;
-        }
-        return `${padding}px`
+        )
     }
 
     renderBasisUnitOptions() {
@@ -125,59 +75,19 @@ class Basis extends React.Component {
             <div className="bgm-dark basis-options btn-group btn-group-justified"
                 data-toggle="buttons"
                 id="basis-options"
-                ref={(e) => {
-                    this.basisOptions = e;
-                }}>
-                <label className={setClass("btn btn-custom rotate active")}
-                    id="basis-units-crystal"
-                    onClick={this.handleBasisUnits}>
-                    <input type="radio"
-                        name="basis.units"
-                        autoComplete="off"
-                        defaultValue="crystal"
-                        defaultChecked="checked"/> Crystal Units
-                </label>
-                <label
-                    className={setClass("btn btn-custom")}
-                    id="basis-units-cartesian"
-                    onClick={this.handleBasisUnits}>
-                    <input type="radio"
-                        name="basis.units"
-                        autoComplete="off"
-                        defaultValue="cartesian"/> Cartesian Units
-                </label>
+                ref={(e) => {this.basisOptions = e}}>
+                {this.renderBasisUnitsLabel(Made.ATOMIC_COORD_UNITS.crystal)}
+                {this.renderBasisUnitsLabel(Made.ATOMIC_COORD_UNITS.cartesian)}
             </div>
         )
     }
 
-    renderBasisTextare() {
+    renderBasisText() {
         return (
-            <div className="xyz"
-                style={{
-                    height: '100%',
-                }}>
-                <div style={{height: '100%'}}>
-                            <textarea name="basis-xyz"
-                                className={setClass("material-textarea form-control fg-input")}
-                                required=""
-                                onFocus={() => {
-                                    this.setState({manualEditStarted: true});
-                                }}
-                                onBlur={(e) => {
-                                    this.setState({manualEditStarted: false});
-                                    this.reformatXYZText();
-                                }}
-                                value={this.state.xyz}
-                                onChange={this.handleMaterialTextareaChange}
-                            />
-                    <pre id="basis-xyz-hidden" className="hidden">{this.props.material.getBasisAsXyz()}</pre>
-                    <div className="col-xs-12 p-5 text-center">
-                        <span className={this.state.validated ? "text-success" : "text-danger"}>
-                            {this.state.message}&nbsp;
-                        </span>
-                    </div>
-                </div>
-            </div>
+            <BasisText
+                content={this.state.xyz}
+                onChange={this.handleBasisTextChange}
+            />
         );
     }
 
@@ -193,7 +103,7 @@ class Basis extends React.Component {
                     height: '100%'
                 }}>
                     <div className="col-xs-12 p-0">{this.renderBasisUnitOptions()}</div>
-                    <div className="col-xs-12 p-0">{this.renderBasisTextare()}</div>
+                    <div className="col-xs-12 p-0">{this.renderBasisText()}</div>
                 </ExpansionPanelDetails>
             </ExpansionPanel>
         )
