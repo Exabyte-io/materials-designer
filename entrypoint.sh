5f5a5141a7607cc31d70c2457658abf83e6f52f4
tjduigna <thomas.j.duignan@gmail.com>
2021-12-27 22:27:32 -0500

    feat: test container builds completely

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ source $NVM_DIR/nvm.sh
 nvm use ${NODE_VERSION}
 
 if [[ "$1" == "test" ]]; then
-    ./tests/node_modules/.bin/chimp \
-        --serverHost="${HOST}" \
-        --serverPort="${PORT}" \
+    xvfb-run -s "-ac -screen 0 1024x768x24" ./tests/node_modules/.bin/chimp \
+        --serverHost="127.0.0.1" \
+        --serverPort="3001" \
         --path=./tests/cucumber/features/ \
         -r=./tests/cucumber/support \
         --singleSnippetPerFile=1 \
@@ -15,7 +15,7 @@ if [[ "$1" == "test" ]]; then
         --browser=chrome \
         --webdriverio.deprecationWarnings=false \
         --webdriverio.logLevel="silent" \
-        --seleniumStandaloneOptions.drivers.chrome.version=${CHROME_VERSION} 
-else 
+        --seleniumStandaloneOptions.drivers.chrome.version=2.35
+else
     npm start
 fi
