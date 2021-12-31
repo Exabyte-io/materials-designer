c6bab732d03c97c066be9b324a8a12caef47b17f
tjduigna <thomas.j.duignan@gmail.com>
2021-12-30 20:50:16 -0500

    feat: update docker-commands for 'all'

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 
 if [[ "$1" == "build" ]]; then
-    if [[ "$2" == "app" ]]; then
-        echo "building app"
-        docker build -t materials-designer:centos -f dockerfiles/centos/Dockerfile .
+    if [[ "$2" == "application" ]]; then
+        echo "building application container"
+        docker build -t materials-designer:application -f dockerfiles/centos/Dockerfile .
     elif [[ "$2" == "test" ]]; then
         echo "building test"
         docker build -t materials-designer:test -f dockerfiles/test/Dockerfile .
     fi
 elif [[ "$1" == "run" ]]; then
-    if [[ "$2" == "app" ]]; then
-        echo "running app"
-        docker run -it -p 3001:3001 -d --name=centos materials-designer:centos
+    if [[ "$2" == "application" ]]; then
+        echo "running application container"
+        docker run -it -p 3001:3001 -d --name=application materials-designer:application
     elif [[ "$2" == "test" ]]; then
         echo "running test"
         docker run -it --network=host --name=test materials-designer:test
     fi
-elif [[ "$1" == "" ]]; then
-    echo "Building app and test, then running app and tests"
-    docker build -t materials-designer:centos -f dockerfiles/centos/Dockerfile .
+elif [[ "$1" == "all" ]]; then
+    echo "Building application and test containers, then running application and test containers"
+    docker build -t materials-designer:application -f dockerfiles/centos/Dockerfile .
     docker build -t materials-designer:test -f dockerfiles/test/Dockerfile .
-    docker run -it -p 3001:3001 -d --name=centos materials-designer:centos
+    docker run -it -p 3001:3001 -d --name=application materials-designer:application
     sleep 30
     docker run -it --network=host --name=test materials-designer:test
 else
-    echo "Supports [build|run] [app|test] only"
+    echo "Supports [build|run] [application|test] containers only"
 fi
