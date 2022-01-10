1328686c3307105c1b02a64424ee00b5747b00bd
tjduigna <thomas.j.duignan@gmail.com>
2022-01-10 14:25:36 -0500

    feat: update source editor but don't convert to function yet

--- src/components/source_editor/SourceEditor.js
+++ src/components/source_editor/SourceEditor.js
@@ -5,44 +5,31 @@ import React from "react";
 import BasisEditor from "./Basis";
 import LatticeEditor from "./Lattice";
 
+// eslint-disable-next line react/prefer-stateless-function
 class SourceEditor extends React.Component {
     render() {
+        const { className, material, onUpdate } = this.props;
         return (
             <div
-                className={setClass(this.props.className, "materials-designer-source-editor")}
+                className={setClass(className, "materials-designer-source-editor")}
                 style={{
                     // TODO: move out of here
                     borderLeft: "1px solid",
                     borderRight: "1px solid",
                 }}
             >
-                <LatticeEditor
-                    className="col-xs-12 p-0"
-                    material={this.props.material}
-                    onUpdate={this.props.onUpdate}
-                />
-                <BasisEditor
-                    className="col-xs-12 p-0"
-                    material={this.props.material}
-                    onUpdate={this.props.onUpdate}
-                />
+                <LatticeEditor className="col-xs-12 p-0" material={material} onUpdate={onUpdate} />
+                <BasisEditor className="col-xs-12 p-0" material={material} onUpdate={onUpdate} />
             </div>
         );
     }
 }
 
 SourceEditor.propTypes = {
-    editable: PropTypes.bool.isRequired,
-    showToolbar: PropTypes.bool,
-
+    // eslint-disable-next-line react/forbid-prop-types
     material: PropTypes.object.isRequired,
-    index: PropTypes.number,
-    length: PropTypes.number,
-
-    onUpdate: PropTypes.func,
-    onAdd: PropTypes.func,
-    onRemove: PropTypes.func,
-    onUpdateIndex: PropTypes.func,
+    className: PropTypes.string.isRequired,
+    onUpdate: PropTypes.func.isRequired,
 };
 
 export default SourceEditor;
