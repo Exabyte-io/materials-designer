86c40ff2548c48f3d0dbd4215e76aba084a03ace
fluoxe7ine <oleksander.kozyriev@gmail.com>
2022-07-28 14:48:51 +0300

    chore: remove redundant console.log

--- tests/cucumber/support/step_definitions/When I toggle scene object # opener inside 3D editor.js
+++ tests/cucumber/support/step_definitions/When I toggle scene object # opener inside 3D editor.js
@@ -2,7 +2,6 @@ import {materialDesignerPage} from "../widgets/material_designer_page";
 
 export default function () {
     this.When(/^I toggle scene object "([^"]*)" opener inside 3D editor$/, function (sceneObjectName) {
-        console.log({ sceneObjectName });
         materialDesignerPage.designerWidget.threeJSEditorWidget.toggleSceneObjectOpener(sceneObjectName);
     });
 }
