9bdc39ca3f8477051cc45a9f0ae07e0e41ae9d94
Anton Slipchenko <anton.slipchenko@plus4u.net>
2022-09-02 01:06:36 +0300

    fix: name of the first material is screwed up

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,6 +1,7 @@
 import _ from "underscore";
 
 export function safeMakeArrayIfNot(x) {
+    // TODO: should here be used js function Array.isArray() ??
     if (!_.isArray(x)) return [x];
     return x;
 }
