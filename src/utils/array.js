030772bee5dc72fbea721cfe894af222459f99e5
Timur Bazhirov <timur.bazhirov@gmail.com>
2022-09-01 19:51:43 -0700

    Update array.js

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,7 +1,7 @@
 import _ from "underscore";
 
 export function safeMakeArrayIfNot(x) {
-    // TODO: should here be used js function Array.isArray() ??
+    // TODO: use code.js and Array.isArray() for this
     if (!_.isArray(x)) return [x];
     return x;
 }
