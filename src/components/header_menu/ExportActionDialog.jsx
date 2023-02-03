7debc0df8d39f715b763c78c8e15a775a9fe1b1e
Yurii Vlasiuk <yuriivlasiuk@gmail.com>
2023-02-03 18:09:25 +0200

    update: run codemods

--- src/components/header_menu/ExportActionDialog.jsx
+++ src/components/header_menu/ExportActionDialog.jsx
@@ -1,7 +1,7 @@
-import FormControl from "@material-ui/core/FormControl";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
+import FormControl from "@mui/material/FormControl";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
 import React from "react";
 
 import { ActionDialog } from "../include/ActionDialog";
--- src/components/include/material-ui/theme.js
+++ src/components/include/material-ui/theme.js
@@ -1,10 +1,11 @@
 import { createTheme } from "@mui/material";
+import { adaptV4Theme } from "@mui/material/styles";
 
 // adopted from: https://material-ui.com/customization/themes/#theme-configuration-variables
 const DarkThemeConfig = {
     palette: {
         // Switching the dark mode on is a single property value change.
-        type: "dark",
+        mode: "dark",
     },
     typography: {
         // Tell Material-UI what's the font-size on the html element is.
@@ -20,11 +21,13 @@ const DarkThemeConfig = {
     },
 };
 
-export const DarkMaterialUITheme = createTheme(DarkThemeConfig);
+export const DarkMaterialUITheme = createTheme(adaptV4Theme(DarkThemeConfig));
 
-export const LightMaterialUITheme = createTheme({
-    typography: DarkThemeConfig.typography,
-    palette: {
-        type: "light",
-    },
-});
+export const LightMaterialUITheme = createTheme(
+    adaptV4Theme({
+        typography: DarkThemeConfig.typography,
+        palette: {
+            mode: "light",
+        },
+    }),
+);
