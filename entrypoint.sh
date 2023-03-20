b2b6aca06c6a4f45b77e4e87643cc5d3b0de3f6f
Yurii Vlasiuk <yuriivlasiuk@gmail.com>
2023-03-20 16:56:31 +0200

    update: Docker files and compose configuration for ci tests run

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,14 +10,16 @@ fi
 export DEBUG_LEVEL=3
 export DEBUG="exachimp:*"
 export ROOT_URL="http://127.0.0.1:3001"
+export DISPLAY=":99"
 
 # Chimp can also be started within an xvfb-run command
 # by prepending the following line to the chimp call.
 # xvfb-run -s "-ac -screen 0 1920x1080x24"
 
 if [[ "$1" == "test" ]]; then
+    Xvfb -ac :99 -screen 0 1280x1024x16 &
     # If "test" is passed as a parameter - running tests/chimp
-    ${BASE_DIR}/tests/node_modules/.bin/chimp \
+    ${BASE_DIR}/node_modules/.bin/chimp \
         --serverHost="http://127.0.0.1" \
         --serverPort="3001" \
         --path=${BASE_DIR}/tests/cucumber/features/ \
@@ -26,12 +28,13 @@ if [[ "$1" == "test" ]]; then
         --screenshotsOnError=true \
         --captureAllStepScreenshots=false \
         --browser=chrome \
+        --seleniumStandaloneOptions.drivers.chrome.version=109.0.5414.25 \
         --webdriverio.deprecationWarnings=false \
         --webdriverio.logLevel="silent" \
         --webdriverio.desiredCapabilities.chromeOptions.args="headless" \
         --webdriverio.desiredCapabilities.chromeOptions.args="disable-gpu" \
-        --webdriverio.desiredCapabilities.chromeOptions.args="no-sandbox" \
-        --webdriverio.desiredCapabilities.chromeOptions.args="window-size=1920,1080"
+        --webdriverio.desiredCapabilities.chromeOptions.args="disable-dev-shm-usage" \
+        --webdriverio.desiredCapabilities.chromeOptions.args="no-sandbox"
 else
     # If "test" is not passed as a parameter - running the application itself
     npm start
