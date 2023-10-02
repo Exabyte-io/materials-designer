b838b711a9712cc2729863206ed7f2f54212ff6a
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-10-02 15:53:26 -0700

    update: list python code map as URLs and literals

--- src/pythonCodeMap.js
+++ src/pythonCodeMap.js
@@ -1,21 +1,38 @@
 const transformationsMap = {
+    default: `def func():
+    """This function returns the material without any change (as conventional cell)"""
+    poscar_data = globals()["data"]["poscar_data"]
+    print("Pyodide poscar_data:", poscar_data)
+    return poscar_data[0]
+func()`,
     createInterface:
         "https://raw.githubusercontent.com/Exabyte-io/api-examples/afc1a490c57ba3fc7c277a501cea7f05c3300061/other/two_dimensional_materials/layer_on_a_surface.py",
 };
 
 async function fetchPythonCode(transformationName) {
-    const url = transformationsMap[transformationName];
-    if (!url) {
-        console.error(`No URL found for transformation: ${transformationName}`);
+    const value = transformationsMap[transformationName];
+
+    if (!value) {
+        console.error(`No value found for transformation: ${transformationName}`);
         return;
     }
 
-    try {
-        const response = await fetch(url);
-        return await response.text();
-    } catch (error) {
-        console.error(`Error fetching code for transformation: ${transformationName}`, error);
+    const urlPattern = /^https?:\/\//;
+
+    if (urlPattern.test(value)) {
+        try {
+            const response = await fetch(value);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return await response.text();
+        } catch (error) {
+            console.error(`Error fetching code for transformation: ${transformationName}`, error);
+            return;
+        }
     }
+
+    return value;
 }
 
 export { fetchPythonCode, transformationsMap };
