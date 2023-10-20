4247ed0c9d8e42ee5927f83f4ff4092986158bc2
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-10-20 12:55:58 -0700

    chore: adjust imports

--- src/MaterialsDesigner.jsx
+++ src/MaterialsDesigner.jsx
@@ -1,4 +1,4 @@
-import FullscreenComponentMixin from "@exabyte-io/cove.js/dist/other/FullscreenComponent";
+import FullscreenComponentMixin from "@exabyte-io/cove.js/dist/other/fullscreen";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
 import setClass from "classnames";
 import { mix } from "mixwith";
