faa7f294fca3aec0d31c4ee165f17bdc9f9eea81
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-10-19 23:01:33 -0700

    feat: use fullscreen mixin from cove

--- src/components/3d_editor/ThreeDEditorFullscreen.jsx
+++ src/components/3d_editor/ThreeDEditorFullscreen.jsx
@@ -1,11 +1,10 @@
+import FullscreenComponentMixin from "@exabyte-io/cove.js/dist/other/FullscreenComponent";
 import { ThreeDEditor } from "@exabyte-io/wave.js";
 import FullscreenIcon from "@mui/icons-material/Fullscreen";
 import setClass from "classnames";
 import { mix } from "mixwith";
 import PropTypes from "prop-types";
 import React from "react";
-
-import { FullscreenComponentMixin } from "../include/FullscreenComponentMixin";
 
 export class ThreeDEditorFullscreen extends mix(ThreeDEditor).with(FullscreenComponentMixin) {
     getToolbarConfig() {
@@ -17,7 +16,6 @@ export class ThreeDEditorFullscreen extends mix(ThreeDEditor).with(FullscreenCom
                 title: "Fullscreen",
                 leftIcon: <FullscreenIcon />,
                 onClick: () => {
-                    clsInstance.setState({ viewerTriggerResize: true });
                     clsInstance.toggleFullscreen();
                 },
             },
