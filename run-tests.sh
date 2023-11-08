16017b5d1276996390fd99e487ff193d75dc03a9
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-11-08 12:49:05 -0800

    chore: fix path for chimpy for debugging

--- run-tests.sh
+++ run-tests.sh
@@ -92,7 +92,7 @@ rm -rf ${SCREENSHOTS_DIR}
 # Hotfix: change node debug option in cucumber
 # This is actually useful for debugging the application in WebStorm
 # https://www.jetbrains.com/help/webstorm/running-and-debugging-node-js.html#node_debugging_overview
-# sed -ie 's/--debug/--inspect/g'  ${TESTS_DIR}/node_modules/chimp/dist/lib/cucumberjs/cucumber.js
+# sed -ie 's/--debug/--inspect/g'  ${TESTS_DIR}/node_modules/chimpy/dist/lib/cucumberjs/cucumber.js
 
 ${TESTS_DIR}/node_modules/.bin/chimp \
     --serverHost="${HOST}" \
