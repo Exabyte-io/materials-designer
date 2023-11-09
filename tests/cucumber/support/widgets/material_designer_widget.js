e8ca60e1a7b2b74e62657b2865e3ad06311110cf
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-11-08 19:59:29 -0800

    fix: test in web-app requiring visibility

--- tests/cucumber/support/widgets/material_designer_widget.js
+++ tests/cucumber/support/widgets/material_designer_widget.js
@@ -40,6 +40,7 @@ export class MaterialDesignerWidget extends Widget {
     }
 
     openSaveDialog() {
+        exabrowser.waitForVisible(SELECTORS.headerMenu.wrapper);
         this.headerMenu.selectMenuItemByNameAndItemNumber("Input/Output", 3);
     }
 
