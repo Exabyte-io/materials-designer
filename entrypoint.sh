abd43f223e001c13e32bac440487d9ed096b483b
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-11-20 19:25:56 -0800

    update: revert docker setup to work on GHA

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,34 +11,31 @@ if [[ "$1" == "test" ]]; then
 
     export DEBUG_LEVEL=3
     export DEBUG="exachimp:*"
-    export ROOT_URL="http://host.docker.internal:3001"
+    export ROOT_URL="http://127.0.0.1:3001"
     export DISPLAY=":99"
 
-    echo "Starting Xvfb"
-    Xvfb $DISPLAY -screen 0 1920x1280x16 &> Xvfb.log &
+    # Chimp can also be started within an xvfb-run command
+    # by prepending the following line to the chimp call.
+    # xvfb-run -s "-ac -screen 0 1920x1080x24"
 
-    echo "Starting x11vnc service"
-    x11vnc -passwd 123 -display $DISPLAY -N -forever &> x11vnc.log &
-
+    Xvfb -ac $DISPLAY -screen 0 1280x1024x16 &
     # If "test" is passed as a parameter - running tests/chimp
     ${BASE_DIR}/node_modules/.bin/chimp \
-        --serverHost="http://host.docker.internal" \
+        --serverHost="http://127.0.0.1" \
         --serverPort="3001" \
         --path=${BASE_DIR}/tests/cucumber/features/ \
         -r=${BASE_DIR}/tests/cucumber/support \
-        --screenshotsOnError=true \
         --singleSnippetPerFile=1 \
         --screenshotsOnError=true \
         --captureAllStepScreenshots=false \
-        --browser chrome \
+        --browser=chrome \
         --seleniumStandaloneOptions.drivers.chrome.version=109.0.5414.25 \
         --webdriverio.deprecationWarnings=false \
         --webdriverio.logLevel="silent" \
-        --webdriverio.desiredCapabilities.chromeOptions.args=disable-gpu \
-        --webdriverio.desiredCapabilities.chromeOptions.args=disable-dev-shm-usage \
-        --webdriverio.desiredCapabilities.chromeOptions.args=no-sandbox \
-
-
+        --webdriverio.desiredCapabilities.chromeOptions.args="headless" \
+        --webdriverio.desiredCapabilities.chromeOptions.args="disable-gpu" \
+        --webdriverio.desiredCapabilities.chromeOptions.args="disable-dev-shm-usage" \
+        --webdriverio.desiredCapabilities.chromeOptions.args="no-sandbox"
 else
     # If "test" is not passed as a parameter - running the application itself
     npm start
