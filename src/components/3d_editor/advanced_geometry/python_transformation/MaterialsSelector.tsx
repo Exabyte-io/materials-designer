faa88872823d5ab25add163203e78c65df489dc7
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-11-24 10:48:23 -0800

    chore: fix margin issue:

--- src/components/3d_editor/advanced_geometry/python_transformation/MaterialsSelector.tsx
+++ src/components/3d_editor/advanced_geometry/python_transformation/MaterialsSelector.tsx
@@ -2,7 +2,6 @@ import CheckBox from "@mui/icons-material/CheckBox";
 import CheckBoxOutlineBlank from "@mui/icons-material/CheckBoxOutlineBlank";
 import Autocomplete from "@mui/material/Autocomplete";
 import Checkbox from "@mui/material/Checkbox";
-import Chip from "@mui/material/Chip";
 import TextField from "@mui/material/TextField";
 import React from "react";
 
@@ -44,16 +43,7 @@ function MaterialsSelector(props: MaterialsSelectorProps) {
                     size="small"
                 />
             )}
-            renderTags={(value, getTagProps) =>
-                value.map((option, index) => (
-                    <Chip
-                        size="small"
-                        label={`${index}: ${option.name}`}
-                        // eslint-disable-next-line react/jsx-props-no-spreading
-                        {...getTagProps({ index })}
-                    />
-                ))
-            }
+            sx={{ "& .MuiAutocomplete-tag": { margin: "0 !important" } }}
         />
     );
 }
