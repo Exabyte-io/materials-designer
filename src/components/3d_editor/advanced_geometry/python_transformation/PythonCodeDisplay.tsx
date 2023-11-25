d0948dca07ce49e6e9335574c6340f0a575cfdf5
Timur Bazhirov <timur@exabyte.io>
2023-11-24 21:46:01 -0800

    chore: ignore the CodeMirror TS error

--- src/components/3d_editor/advanced_geometry/python_transformation/PythonCodeDisplay.tsx
+++ src/components/3d_editor/advanced_geometry/python_transformation/PythonCodeDisplay.tsx
@@ -11,7 +11,7 @@ interface PythonCodeDisplayProps {
 
 const PythonCodeDisplay = (props: PythonCodeDisplayProps) => {
     const { pythonCode, pythonOutput, setPythonCode } = props;
-
+    // @ts-ignore
     return (
         <>
             <Box
@@ -20,18 +20,29 @@ const PythonCodeDisplay = (props: PythonCodeDisplayProps) => {
                     border: `1px solid ${theme.palette.secondary.light}`,
                 }}
             >
-                <CodeMirror
-                    content={pythonCode}
-                    updateContent={(newContent: string) => setPythonCode(newContent)}
-                    options={{
-                        lineNumbers: true,
-                    }}
-                    theme="light"
-                    language="python"
-                />
+                {pythonCode && (
+                    // eslint-disable-next-line no-irregular-whitespace
+                    /* Figure out how to fix errors and remove ts-ignore:
+                       JSX element class does not support attributes because it does not have a 'props' property.
+                       'CodeMirror' cannot be used as a JSX component.  instance type 'CodeMirror' is
+                        not a valid JSX element. type 'CodeMirror' is missing the following properties
+                        from type 'ElementClass': context, setState, forceUpdate, props, and 2 more.
+                    */
+                    // @ts-ignore
+                    <CodeMirror
+                        content={pythonCode}
+                        updateContent={(newContent: string) => setPythonCode(newContent)}
+                        options={{
+                            lineNumbers: true,
+                        }}
+                        theme="light"
+                        language="python"
+                    />
+                )}
             </Box>
             <Box id="python-output" mt={theme.spacing(1)}>
                 {pythonOutput && (
+                    // @ts-ignore
                     <CodeMirror
                         content={pythonOutput}
                         readOnly
