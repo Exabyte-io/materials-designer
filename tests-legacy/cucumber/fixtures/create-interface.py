801eac95c0b510ff1839ba6d83580a09201f3df0
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-12-08 21:45:03 -0800

    tests: update with new correct interface creation

--- tests-legacy/cucumber/fixtures/create-interface.py
+++ tests-legacy/cucumber/fixtures/create-interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fbd0b0e67f13d621a00367e55982b18a03fef148d1b5f69be151979bff00d98
-size 4581
+oid sha256:185180b9a6d4dfb2c4d730ad4d24ddd6b79b02a4248ece919ceab80568aa23cb
+size 6163
