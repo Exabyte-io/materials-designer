895c2a19f906f1a5054b6967ef54ce697094fae0
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-12-11 16:07:14 -0800

    update: add datagrid and selection handling

--- src/components/include/StandataImportModal.tsx
+++ src/components/include/StandataImportModal.tsx
@@ -1,8 +1,11 @@
 import Dialog from "@exabyte-io/cove.js/dist/mui/components/dialog/Dialog";
 import { Made } from "@exabyte-io/made.js";
+import DeleteIcon from "@mui/icons-material/Delete";
 import Autocomplete from "@mui/material/Autocomplete";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import React from "react";
 
 interface StandataImportModalProps {
@@ -13,6 +16,7 @@ interface StandataImportModalProps {
 }
 
 interface StandataImportModalState {
+    selectedMaterial: Made.Material | null;
     selectedMaterials: Made.Material[];
 }
 
@@ -23,50 +27,101 @@ class StandataImportModal extends React.Component<
     constructor(props: StandataImportModalProps) {
         super(props);
         this.state = {
+            selectedMaterial: null,
             selectedMaterials: [],
         };
     }
 
-    addMaterialsAsJSONFile = () => {
+    handleMaterialSelect = (material: Made.Material | null) => {
+        if (material) {
+            this.setState((prevState) => ({
+                selectedMaterials: [...prevState.selectedMaterials, material],
+                selectedMaterial: null,
+            }));
+        }
+    };
+
+    handleRemoveMaterial = (id: number) => {
+        this.setState((prevState) => ({
+            selectedMaterials: prevState.selectedMaterials.filter((_, index) => index !== id),
+        }));
+    };
+
+    addMaterials = () => {
         const { selectedMaterials } = this.state;
         const newMaterials = selectedMaterials.map((material) => new Made.Material(material));
         const { onSubmit } = this.props;
         onSubmit(newMaterials);
+        this.setState({ selectedMaterials: [] });
     };
 
     render() {
-        const { show, onClose, defaultMaterialsSet } = this.props;
-        const { selectedMaterials } = this.state;
-        const defaultMaterialsList = defaultMaterialsSet;
+        const { show, onClose, defaultMaterialsSet } = this.props,
+            { selectedMaterial, selectedMaterials } = this.state;
+
+        const columns: GridColDef[] = [
+            { field: "name", headerName: "Name", flex: 1 },
+            { field: "lattice-type", headerName: "Lattice", flex: 1 },
+            { field: "formula", headerName: "Formula", flex: 1 },
+            {
+                field: "actions",
+                headerName: "Actions",
+                flex: 0.5,
+                sortable: false,
+                filterable: false,
+                disableColumnMenu: true,
+                renderCell: (params) => (
+                    <Button
+                        id={`${params.row.name.replace(/\s+/g, "-")}-remove-button`}
+                        variant="text"
+                        color="warning"
+                        size="small"
+                        onClick={() => this.handleRemoveMaterial(params.row.id)}
+                    >
+                        Remove
+                    </Button>
+                ),
+            },
+        ];
 
         return (
             <Dialog
                 open={show}
                 id="standataImportModalDialog"
-                title="Import Default Materials"
+                title="Import from Standata"
                 onClose={onClose}
-                onSubmit={this.addMaterialsAsJSONFile}
+                onSubmit={this.addMaterials}
             >
-                <div>
-                    <Autocomplete
-                        disablePortal
-                        multiple
-                        options={defaultMaterialsList}
-                        getOptionLabel={(material) => material.name || "Not available"}
-                        value={selectedMaterials}
-                        onChange={(event, value) => this.setState({ selectedMaterials: value })}
-                        renderInput={(params) => (
-                            // eslint-disable-next-line react/jsx-props-no-spreading
-                            <TextField {...params} label="Default set of Materials" />
-                        )}
-                    />
-                    <Button
-                        onClick={this.addMaterialsAsJSONFile}
-                        disabled={!selectedMaterials.length}
-                    >
-                        Import
-                    </Button>
-                </div>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Autocomplete
+                            disablePortal
+                            size="small"
+                            options={defaultMaterialsSet}
+                            getOptionLabel={(material) => material.name || "Not available"}
+                            value={selectedMaterial}
+                            onChange={(event, newValue) => this.handleMaterialSelect(newValue)}
+                            renderInput={(params) => (
+                                // eslint-disable-next-line react/jsx-props-no-spreading
+                                <TextField {...params} label="Select Material" />
+                            )}
+                        />
+                    </Grid>
+                    <Grid item xs={12} style={{ height: 400 }}>
+                        <DataGrid
+                            data-name="data-grid"
+                            hideFooter
+                            rows={selectedMaterials.map((material, index) => ({
+                                ...material,
+                                id: index,
+                            }))}
+                            columns={columns}
+                            pageSize={5}
+                            rowsPerPageOptions={[5]}
+                            disableSelectionOnClick
+                        />
+                    </Grid>
+                </Grid>
             </Dialog>
         );
     }
