afd674184b3c0f769c50680d3ec314fc79d86476
Timur Bazhirov <timur@exabyte.io>
2023-12-18 11:05:39 -0800

    chore: compare strings after trimming

--- tests/cypress/support/step_definitions/I see code output with the following data.ts
+++ tests/cypress/support/step_definitions/I see code output with the following data.ts
@@ -1,11 +1,10 @@
 import { Then } from "@badeball/cypress-cucumber-preprocessor";
 
-import { shallowDeepAlmostEqual } from "../utils";
 import { materialDesignerPage } from "../widgets/MaterialDesignerPage";
 
 Then("I see code output with the following data", (docString: string) => {
     const { pythonTransformationDialog } = materialDesignerPage.designerWidget;
-    pythonTransformationDialog.getCode(0).then((actualContent) => {
-        shallowDeepAlmostEqual(docString, actualContent);
+    pythonTransformationDialog.getCode(0).then((actualContent: string) => {
+        expect(docString.trim()).to.equal(actualContent.trim());
     });
 });
