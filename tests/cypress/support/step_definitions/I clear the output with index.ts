fcd29ab37b49c8f5b44a807b74d7cea6d32ba71c
Timur Bazhirov <timur@exabyte.io>
2023-12-18 10:56:04 -0800

    chore: use index as string in test step

--- tests/cypress/support/step_definitions/I clear the output with index.ts
+++ tests/cypress/support/step_definitions/I clear the output with index.ts
@@ -2,6 +2,6 @@ import { When } from "@badeball/cypress-cucumber-preprocessor";
 
 import { materialDesignerPage } from "../widgets/MaterialDesignerPage";
 
-When("I clear the output with index {number}", (index: number) => {
-    materialDesignerPage.designerWidget.pythonTransformationDialog.clearOutput(index);
+When("I clear the output with index {string}", (index: string) => {
+    materialDesignerPage.designerWidget.pythonTransformationDialog.clearOutput(parseInt(index, 10));
 });
