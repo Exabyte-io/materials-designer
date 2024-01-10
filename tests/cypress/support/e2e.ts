10b83301ce3bdc8c991acd71f861513dde4c6e58
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-01-10 17:56:41 +0200

    chore: use updated browser factory

--- tests/cypress/support/e2e.ts
+++ tests/cypress/support/e2e.ts
@@ -12,9 +12,13 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
 // Import commands.js using ES2015 syntax:
 import "./commands";
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+import BrowserFactory from "@exabyte-io/code.js/dist/cypress/BrowserFactory";
+
+import SETTINGS from "./settings";
+
+beforeEach(() => {
+    BrowserFactory.setBrowserSettings(SETTINGS);
+});
--- tests/cypress/support/widgets/Widget.ts
+++ tests/cypress/support/widgets/Widget.ts
@@ -1,9 +1,4 @@
-import BrowserFactory from "@exabyte-io/code.js/dist/cypress/BrowserFactory";
 import BaseWidget from "@exabyte-io/code.js/dist/cypress/Widget";
-
-import SETTINGS from "../settings";
-
-BrowserFactory.setBrowserSettings(SETTINGS);
 
 export default class Widget extends BaseWidget {
     // Put custom actions here
