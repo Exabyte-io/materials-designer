fa1a03c0a7fada45c0c12698f0260e6d00f49278
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-01-26 16:48:29 -0800

    chore: cleanup

--- src/components/3d_editor/advanced_geometry/python_transformation/JupyterLiteTransformation.tsx
+++ src/components/3d_editor/advanced_geometry/python_transformation/JupyterLiteTransformation.tsx
@@ -1,18 +1,13 @@
 import Dialog from "@exabyte-io/cove.js/dist/mui/components/dialog/Dialog";
-import IconByName from "@exabyte-io/cove.js/dist/mui/components/icon/IconByName";
 import { Made } from "@exabyte-io/made.js";
 import { darkScrollbar } from "@mui/material";
-import Button from "@mui/material/Button";
 import DialogContent from "@mui/material/DialogContent";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import React from "react";
 import NPMsAlert from "react-s-alert";
 
 import { theme } from "../../../../settings";
-import CodeExecutionControls, { ExecutionStatus } from "./CodeExecutionControls";
 import MaterialsSelector from "./MaterialsSelector";
 
 interface JupyterLiteTransformationProps {
@@ -24,11 +19,10 @@ interface JupyterLiteTransformationProps {
 
 interface JupyterLiteTransformationState {
     materials: Made.Material[];
-    selectedMaterials: Made.Material[];
     newMaterials: Made.Material[];
-    executionStatus: ExecutionStatus;
 }
 
+// TODO: set to an actual URL during deployment
 const ORIGIN_URL = "http://localhost:8000";
 const LOCAL_URL = "http://localhost:3001";
 const IFRAME_ID = "jupyter-lite-iframe";
@@ -41,31 +35,19 @@ class JupyterLiteTransformation extends React.Component<
         super(props);
         this.state = {
             materials: props.materials,
-            selectedMaterials: [props.materials[0]],
             newMaterials: [],
-            executionStatus: ExecutionStatus.Ready,
         };
     }
 
     componentDidMount() {
         window.addEventListener("message", this.handleReceiveMessage, false);
-        const iframe = document.getElementById(IFRAME_ID);
-        console.log("iframe", iframe);
     }
 
-    componentDidUpdate(
-        prevProps: JupyterLiteTransformationProps,
-        prevState: JupyterLiteTransformationState,
-    ) {
+    componentDidUpdate(prevProps: JupyterLiteTransformationProps) {
         const { materials } = this.props;
         if (prevProps.materials !== materials) {
             // eslint-disable-next-line react/no-did-update-set-state
             this.setState({ materials });
-        }
-
-        const { selectedMaterials } = this.state;
-        if (prevState.selectedMaterials !== selectedMaterials) {
-            this.sendMessageToIFrame();
         }
     }
 
@@ -74,7 +56,6 @@ class JupyterLiteTransformation extends React.Component<
     }
 
     handleReceiveMessage = (event: any) => {
-        // Check if the message is from the expected source
         if (event.origin !== ORIGIN_URL) {
             return;
         }
@@ -82,7 +63,6 @@ class JupyterLiteTransformation extends React.Component<
             console.log("Received data from JupyterLite:", event.data, event.origin);
             try {
                 if (event.data.materials) {
-                    // convert array of JSON into array of Made.Materials
                     const newMaterials = event.data.materials.map((materialConfig: any) => {
                         const newMaterial = new Made.Material(materialConfig);
                         return newMaterial;
@@ -97,11 +77,11 @@ class JupyterLiteTransformation extends React.Component<
     };
 
     handleSubmit = async () => {
-        const { onSubmit, materials } = this.props;
+        const { onSubmit } = this.props;
         const { newMaterials } = this.state;
 
         onSubmit(newMaterials);
-        this.setState({ selectedMaterials: [materials[0]], newMaterials: [] });
+        this.setState({ newMaterials: [] });
     };
 
     sendMessageToIFrame() {
@@ -118,7 +98,7 @@ class JupyterLiteTransformation extends React.Component<
         const postMessage = () => {
             if (iframe.contentWindow) {
                 iframe.contentWindow.postMessage(message, ORIGIN_URL);
-                console.log("Sending message to JupyterLite:", message);
+                console.debug("Sending message to JupyterLite:", message);
             }
         };
 
@@ -130,7 +110,7 @@ class JupyterLiteTransformation extends React.Component<
     }
 
     render() {
-        const { newMaterials, executionStatus } = this.state;
+        const { newMaterials } = this.state;
         const { show, onHide } = this.props;
 
         return (
@@ -142,7 +122,7 @@ class JupyterLiteTransformation extends React.Component<
                 maxWidth="xl"
                 onSubmit={this.handleSubmit}
                 title="Jupyter Lite Transformation"
-                isSubmitButtonDisabled={executionStatus !== ExecutionStatus.Ready}
+                isSubmitButtonDisabled={newMaterials.length === 0}
             >
                 <DialogContent
                     sx={{
@@ -156,16 +136,15 @@ class JupyterLiteTransformation extends React.Component<
                         id="python-transformation-dialog-content"
                         sx={{ height: "100%" }}
                     >
-                            {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
-                            <iframe
-                                name="jupyterlite"
-                                id={IFRAME_ID}
-                                src={ORIGIN_URL}
-                                sandbox="allow-scripts allow-same-origin allow-popups allow-forms allow-modals allow-top-navigation-by-user-activation"
-                                width="100%"
-                                height="100%"
-                            />
-                        </Grid>
+                        <iframe
+                            name="jupyterlite"
+                            title="JupyterLite"
+                            id={IFRAME_ID}
+                            src={ORIGIN_URL}
+                            sandbox="allow-scripts allow-same-origin allow-popups allow-forms allow-modals allow-top-navigation-by-user-activation"
+                            width="100%"
+                            height="100%"
+                        />
                         <Grid item container xs={12} md={4} alignItems="center">
                             <Typography variant="subtitle1">
                                 Output Materials (<code>materials_out</code>)
