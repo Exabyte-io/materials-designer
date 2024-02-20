43f0d9e4a73aa763c8be17f923be5c0b1f71fa27
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-02-20 13:22:40 -0800

    chore: revert

--- tests/cypress/support/step_definitions/material with following data exists in state.ts
+++ tests/cypress/support/step_definitions/material with following data exists in state.ts
@@ -15,30 +15,16 @@ interface Params {
 Given("material with following data exists in state", (table: DataTable) => {
     const config = parseTable<Params>(table)[0];
 
-    const assertMaterialExists = (material: Made.Material) => {
+    const assertMaterialExists = (material: object) => {
         BrowserManager.getBrowser()
             .execute((win) => {
+                // @ts-ignore
                 return win.MDContainer.store
                     .getState()
                     .present.materials.map((m: Made.Material) => m.toJSON());
             })
             .then((materials: Made.Material[]) => {
                 // TODO: fix toJSON() method in made.js to return basis.elements and basis.coordinates array with index starting from 1
-                materials = materials.map((material, id) => {
-                    if (material.basis && material.basis.elements) {
-                        material.basis.elements = material.basis.elements.map((element, i) => {
-                            return { ...element, id: i + 1 };
-                        });
-                    }
-                    if (material.basis && material.basis.coordinates) {
-                        material.basis.coordinates = material.basis.coordinates.map(
-                            (coordinate, i) => {
-                                return { ...coordinate, id: i + 1 };
-                            },
-                        );
-                    }
-                    return material;
-                });
                 shallowDeepAlmostEqual(material, materials[config.index - 1]);
             });
     };
