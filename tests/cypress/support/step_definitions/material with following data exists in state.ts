41e8da96410740e424f78f2c9a172b6a098bc60f
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-02-20 12:22:07 -0800

    chore: temporary fix JSON conversion in tests

--- tests/cypress/support/step_definitions/material with following data exists in state.ts
+++ tests/cypress/support/step_definitions/material with following data exists in state.ts
@@ -1,4 +1,5 @@
 import { DataTable, Given } from "@badeball/cypress-cucumber-preprocessor";
+import { Made } from "@exabyte-io/made.js";
 import BrowserManager from "@mat3ra/tede/src/js/cypress/BrowserManager";
 import { shallowDeepAlmostEqual } from "@mat3ra/tede/src/js/cypress/utils";
 import { parseTable } from "@mat3ra/tede/src/js/cypress/utils/table";
@@ -14,18 +15,27 @@ interface Params {
 Given("material with following data exists in state", (table: DataTable) => {
     const config = parseTable<Params>(table)[0];
 
-    const assertMaterialExists = (material) => {
+    const assertMaterialExists = (material: Made.Material) => {
         BrowserManager.getBrowser()
             .execute((win) => {
-                return win.MDContainer.store.getState().present.materials.map((m) => m.toJSON());
+                return win.MDContainer.store
+                    .getState()
+                    .present.materials.map((m: Made.Material) => m.toJSON());
             })
-            .then((materials) => {
-                // TODO: fix toJSON() method in made.js to return basis.elements array with index starting from 1
-                materials = materials.map((material, index) => {
+            .then((materials: Made.Material[]) => {
+                // TODO: fix toJSON() method in made.js to return basis.elements and basis.coordinates array with index starting from 1
+                materials = materials.map((material, id) => {
                     if (material.basis && material.basis.elements) {
                         material.basis.elements = material.basis.elements.map((element, i) => {
-                            return { ...element, index: i + 1 };
+                            return { ...element, id: i + 1 };
                         });
+                    }
+                    if (material.basis && material.basis.coordinates) {
+                        material.basis.coordinates = material.basis.coordinates.map(
+                            (coordinate, i) => {
+                                return { ...coordinate, id: i + 1 };
+                            },
+                        );
                     }
                     return material;
                 });
